fe4ad3a0bbdedb30362f8bae694089fb8c1d144b
lobo lepe <d.alconerolepe@gmail.com>
2023-11-12 21:35:35 -0600

    ejer 2

--- PAT_Parcial_Reposicion/Ejercicio02.cpp
+++ PAT_Parcial_Reposicion/Ejercicio02.cpp
@@ -2,7 +2,7 @@
 
 Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k)
 {
-   /* Node<char>* tmp = head;
+    Node<char>* tmp = head;
     Node<char>* tmp1 = head;
     Node<char>* tmp2 = nullptr;
     int  n = 0;
@@ -20,13 +20,13 @@ Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k)
         z = 0;
         n = k * (i + 1);
         tmp1 = head;
-        while (z < n && tmp1) 
+        while (z < n && tmp1!=nullptr) 
         {
             tmp1 = tmp1->next;
             z++;
         }
         z = 0;
-        while (z < n && tmp2 && tmp1)
+        while (z < n && tmp2 != nullptr && tmp1 != nullptr)
         {
             tmp2 = tmp->next;
             tmp->next = tmp1;
@@ -35,6 +35,6 @@ Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k)
             z++;
         }
         i++;
-    }*/
-    return nullptr;
+    }
+    return head;
 }
